51e83d424766869935c159ca78c50b82f7ac9c2a
Rishabh Singh <103645516+beingRish@users.noreply.github.com>
2022-11-03 00:15:07 +0530

    Add files via upload

---
+++ Assignment1/10.py
@@ -0,0 +1,3 @@
+#10. Write a python script to print values of three variables, each in a new line. Variable contains integer values.
+a, b, c = 34, 654, 65
+print(a,b,c,sep="\n")
---
+++ Assignment1/9.py
@@ -0,0 +1,4 @@
+#9. Write a python script to create variables to store your name, age, qualification, years of experience and print it.
+n, a, q, yoe = "Rishabh Singh", 24, "BCA", 5
+print("Name=", n, "\nAge=", a, "\nQualification=", q, "\nYear of Experience=", yoe)
+
