5cde78a923f1a83a9567831e1ca67c9af87f02e7
Rishabh Kumar <103645516+beingRish@users.noreply.github.com>
2022-11-06 12:04:01 +0530

    Update 7.js

--- Assignment-34/7.js
+++ Assignment-34/7.js
@@ -4,11 +4,71 @@ b. DD-MM-YYYY HH:mm
 c. DD/MM/YYYY HH:mm */
 
 
-let dateTime1 = new Date('2022-10-30   12:34');
-console.log(dateTime1);
 
-let dateTime2 = new Date('04-09-2022   12:34');
-console.log(dateTime2);
+/*function humanRedableTime1(num) {
+    return num.toString().padStart(2, '0');
+  }
+  
+  function formatDate(date) {
+    return (
+      [
+        date.getFullYear(),
+        humanRedableTime1(date.getMonth() + 1),
+        humanRedableTime1(date.getDate()),
+      ].join('-') +
+      ' ' +
+      [
+        humanRedableTime1(date.getHours()),
+        humanRedableTime1(date.getMinutes()),
+        //humanRedableTime1(date.getSeconds()), This step is optional 
+      ].join(':')
+    );
+  }
+  
+  //  2021-10-24 16:21:23 (yyyy-mm-dd hh:mm:ss)
+  console.log(formatDate(new Date()));*/
+  
 
-let dateTime3 = new Date('09/12/2022   12:34');
-console.log(dateTime3);
+  /*function humanRedableTime2(num) {
+    return num.toString().padStart(2, '0');
+  }
+  function formateDate(date){
+    return [
+        humanRedableTime2(date.getDate()),
+        humanRedableTime2(date.getMonth()+1),
+        date.getFullYear(),
+    ].join('-') +
+    ' '+
+    [
+        humanRedableTime2(date.getHours()),
+        humanRedableTime2(date.getMinutes()),
+        //humanRedableTime2(date.getSeconds()), This step is optional 
+    ].join(':')
+    
+  }
+  // DD-MM-YYYY HH:mm 
+  console.log(formateDate(new Date())); */
+
+
+  function humanRedableTime3(num) {
+    return num.toString().padStart(2, '0');
+  }
+
+  function formateDate(date){
+    return [
+        humanRedableTime3(date.getDate()),
+        humanRedableTime3(date.getMonth()+1),
+        date.getFullYear(),
+    ].join('/') +
+    ' '+
+
+    [
+        humanRedableTime3(date.getHours()),
+        humanRedableTime3(date.getMinutes()),
+        //humanRedableTime3(date.getSeconds()), This step is optional 
+    ].join(':')
+    
+  }
+
+  // DD/MM/YYYY HH:mm 
+  console.log(formateDate(new Date()));
