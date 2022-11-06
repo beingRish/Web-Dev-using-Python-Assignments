9338734b4b993036fd421934ca6cc1563ac7ec1f
Rishabh Kumar <103645516+beingRish@users.noreply.github.com>
2022-11-06 11:28:52 +0530

    Update 2.js

--- Assignment-35/2.js
+++ Assignment-35/2.js
@@ -1,31 +1,11 @@
 /*2. In the webTechs array check if Sass exists in the array and if it exists print 'Sass is a
 CSS preprocess'. If it does not exist add Sass to the array and print the array. */
 
-let webTech = [
-    "HTML", 
-    "CSS", 
-    "Tailwind", 
-    "JavaScript", 
-    "ReactJs", 
-    "Nodejs", 
-    "Express", 
-    "MongoDB",
-];
-let isPresent;
-for(let i = 0; i <= webTech.length; i++){
-    if(webTech[i]=="Sass"){
-        isPresent = true;
-    }
-    else{
-        isPresent = false;
-    }
-}
-
-if(isPresent == true){
-    console.log("Sass is CSS preprocess.");
-}
-else{
-        newlist = webTech.push('Sass');
-        console.log(webTech);
+let webTech = [' is a CSS preprocess']
+let i=['Sass'];
+if ("Sass" in webTech){
+    console.log(webTech);
+}else{
+    console.log(i+[webTech]);
 }
 
